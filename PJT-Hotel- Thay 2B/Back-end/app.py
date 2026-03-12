a6a2b30650098dd9893b05d3bfcc812417e605e9
sheraxy <thayna.covalski@escola.pr.gov.br>
2026-03-12 08:42:20 -0300

    Add files via upload

---
+++ PJT-Hotel- Thay 2B/Back-end/app.py
@@ -0,0 +1,69 @@
+import os
+from flask import Flask, send_from_directory
+import openpyxl  # 📊 Biblioteca para ler e escrever planilhas Excel (.xlsx)
+from datetime import (
+    datetime,
+)  # ⏰ Para registrar a data de cada cadastro automaticamente
+
+
+# Caminho base do projeto (uma pasta acima do backend)
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+# Pasta frontend (HTML e JS)
+FRONTEND_DIR = os.path.join(BASE_DIR, "frontend")
+
+# Pasta static (CSS)
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+
+DB_DIR = os.path.joi(os.path.dirname(__file__), "..", "db")
+EXCEL_FILE = os.path.join(DB_DIR, "Clientes.xlsx")
+
+COLUMNS = [
+    "ID",
+    "Nome,"
+    "CPF,"
+    "Email",
+    "Telefone",
+    "Endereço"
+    "Observações",
+    "Data Cadastro",
+]
+
+def init_excel():
+    if not os.path.exists(DB_DIR):
+        os.makedirs(DB_DIR)
+
+    if not os.path.exists(EXCEL_FILE):
+        workbook = openpyxl.workbook()
+        sheet = workbook.active
+        sheet.title = "Clientes"
+        sheet.append(COLUMNS)
+        workbook.save(EXCEL_FILE)
+
+
+
+app = Flask(__name__, static_folder=STATIC_DIR, static_url_path="/" + STATIC_DIR)
+
+@app.route("/")
+def home():
+    return send_from_directory(FRONTEND_DIR, "index.html")
+
+
+# Página de Consulta
+@app.route("/consulta")
+def consulta_page():
+    return send_from_directory(FRONTEND_DIR, "consulta.html")
+
+
+# Página de Alteração
+@app.route("/alterar")
+def alterar_page():
+    return send_from_directory(FRONTEND_DIR, "alterar.html")
+
+
+if __name__ == "__main__":
+    print("BASE_DIR:", BASE_DIR)
+    print("FRONTEND_DIR:", FRONTEND_DIR)
+    print("STATIC_DIR:", STATIC_DIR)
+    init_excel()
+    app.run(debug=True)
